29c243ee7885f795c120e75c8969bbc8cd4c1f26
MaksJemes <maksimsvin@vk.com>
2023-10-15 15:14:39 +0300

    added homework task 8

---
+++ task8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// // 8 -> 2, 4, 6, 8
+Console.Clear();
+Console.Write("Введите число - ");
+int num1 = int.Parse(Console.ReadLine());
+int number = 0;
+while(number < num1-4)
+{
+    number = number + 2;
+    Console.Write(number + ", ");
+}
+if(number == num1-4) 
+{
+    number = number + 2;
+    Console.Write(number + ".");
+}
