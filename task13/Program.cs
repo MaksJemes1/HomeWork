8dbc01000e1720168998a2dd3e42ab7d89ec6535
MaksJemes <maksimsvin@vk.com>
2023-10-16 20:58:00 +0300

    Delete trash

--- task13/Program.cs
+++ task13/Program.cs
@@ -11,16 +11,10 @@ if(num1 >= 0 && num1 < 100)
 {
    Console.Write("Третьего числа нет!");
 }
-// else if(num1 > 99 && num1 < 1000)
-// {
-//    result = num1 % 10;
-//    Console.Write("Третьим числом является - " + result);
-// } 
 while(result > 999)
 {
    result = result / 10;
    sum = result;
-   Console.WriteLine("Получилось - " + sum);
 }
 if(sum < 1000)
 {
